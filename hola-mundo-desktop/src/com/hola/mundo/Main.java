a80442598e04e443d35e33c9a69e2e60654cac63
RoflDog <rofldogstudios@groups.live.com>
2013-10-20 18:20:53 -0500

    Proyecto generado

---
+++ hola-mundo-desktop/src/com/hola/mundo/Main.java
@@ -0,0 +1,16 @@
+package com.hola.mundo;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "hola-mundo";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new MyGame(), cfg);
+	}
+}
